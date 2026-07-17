05c36814d031571c8d0ed5ed218d3ee727d723ee
MerveKacaran <156471042+MerveKacaran@users.noreply.github.com>
2026-07-17 23:10:06 +0300

    Update ticker.py

--- app/services/ticker.py
+++ app/services/ticker.py
@@ -4,11 +4,13 @@ BASE_URL = "https://api.btcturk.com/api/v2"
 
 
 def get_ticker():
+    """
+    BTCTürk'teki tüm ticker verilerini getirir.
+    """
 
     url = f"{BASE_URL}/ticker"
 
     response = requests.get(url, timeout=10)
-
     response.raise_for_status()
 
     return response.json()["data"]
