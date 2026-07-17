1c1c334abe924f0c213887c3e19971e984b76077
MerveKacaran <156471042+MerveKacaran@users.noreply.github.com>
2026-07-17 22:48:01 +0300

    Update btcturk.py

--- services/btcturk.py
+++ services/btcturk.py
@@ -1,1 +1,17 @@
+import requests
 
+BASE_URL = "https://api.btcturk.com/api/v2"
+
+
+def get_markets():
+    """
+    BTCTürk işlem çiftlerini getirir.
+    """
+
+    url = f"{BASE_URL}/server/exchangeinfo"
+
+    response = requests.get(url, timeout=10)
+
+    response.raise_for_status()
+
+    return response.json()
