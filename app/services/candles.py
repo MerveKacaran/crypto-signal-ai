e28f6c2ee67e2a6920594f183099eb55c68e024e
MerveKacaran <156471042+MerveKacaran@users.noreply.github.com>
2026-07-18 00:40:46 +0300

    Update candles.py

--- app/services/candles.py
+++ app/services/candles.py
@@ -1,15 +1,35 @@
+import time
 import requests
 
 BASE_URL = "https://api.btcturk.com/api/v2"
 
 
-def get_candles(pair="BTCUSDT", interval="1h"):
+def get_candles(symbol="BTCUSDT", resolution="60", limit=200):
     """
-    Şimdilik örnek fonksiyon.
-    Daha sonra BTCTürk API'nin desteklediği candle endpoint'ine göre geliştireceğiz.
+    BTCTürk'ten son mum verilerini getirir.
+
+    resolution:
+    1   = 1 dakika
+    5   = 5 dakika
+    15  = 15 dakika
+    60  = 1 saat
+    240 = 4 saat
+    1440 = 1 gün
     """
-    return {
-        "pair": pair,
-        "interval": interval,
-        "message": "Candles servisi hazır."
-    }
+
+    end = int(time.time())
+
+    start = end - (limit * int(resolution) * 60)
+
+    url = (
+        f"{BASE_URL}/ohlc?"
+        f"pairSymbol={symbol}"
+        f"&from={start}"
+        f"&to={end}"
+        f"&resolution={resolution}"
+    )
+
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+
+    return response.json()["data"]
