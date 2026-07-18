e0465a9a4d85569f3af46a738dcfe2b8bc8b07a4
MerveKacaran <156471042+MerveKacaran@users.noreply.github.com>
2026-07-18 22:32:22 +0300

    Update signals.py

--- app/api/signals.py
+++ app/api/signals.py
@@ -8,3 +8,25 @@ router = APIRouter(prefix="/signals", tags=["Signals"])
 @router.get("/live")
 def live():
     return latest_signals
+
+from typing import List
+
+from app.schemas.signal import SignalResponse
+
+from fastapi import APIRouter
+
+from app.background.scanner import latest_signals
+
+router = APIRouter(
+    prefix="/signals",
+    tags=["Signals"]
+)
+
+
+@router.get(
+    "/live",
+    response_model=List[SignalResponse]
+)
+def live():
+
+    return latest_signals
